d7a5517f983c693223332d3cb36dc60e6f367c2e
Duong Van Trung <trungdvph19910@fpt.edu.vn>
2023-09-25 16:10:22 +0700

    Create GetAllProduct

--- src/component/product.js
+++ src/component/product.js
@@ -16,4 +16,25 @@ const connect = require('../../database');
         return res.status(500).json({ message: 'Loi api', err })
     }
 }
-module.exports = { GetALlBook };
+const getAllProducts = async (req, res)=>{
+    try {
+        const page = req.query.page || 1; // Trang mặc định là 1
+        const perPage = 3; // Số sản phẩm trên mỗi trang
+
+        const offset = (page - 1) * perPage; // Tính toán offset để truy vấn dữ liệu phù hợp
+
+        const sql = 'SELECT * FROM product LIMIT $1 OFFSET $2';
+        const values = [perPage, offset];
+
+        connect.query(sql, values, (err, result) => {
+            if (err) {
+                return res.status(500).json({ message: 'Không lấy được danh sách sản phẩm' });
+            }
+            const products = result.rows;
+            return res.status(200).json({ message: `Danh sách sản phẩm trang ${page}`, products });
+        });
+    } catch (error) {
+        return res.status(500).json({ message: 'Lỗi API' });
+    }
+}
+module.exports = { GetALlBook , getAllProducts};
--- src/router/product.js
+++ src/router/product.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { GetALlBook } = require("../component/product");
+const { GetALlBook, getAllProducts } = require("../component/product");
 const router = express.Router();
 
 router.get("/color", GetALlBook);
+router.get("/product", getAllProducts)
 module.exports = router;
