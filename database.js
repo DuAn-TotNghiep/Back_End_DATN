bdb75625fc394410e23566ce647bcc9f667c722c
Duong Van Trung <trungdvph19910@fpt.edu.vn>
2023-09-25 22:29:57 +0700

    update addcolor

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg');
 const connect = new Client({
     user: 'postgres',
-    password: 'admin',
+    password: 'ADMIN',
     host: 'localhost',
     port: 5432, // Cổng mặc định của PostgreSQL là 5432
-    port: 3001, // Cổng mặc định của PostgreSQL là 5432
+    // port: 3001, // Cổng mặc định của PostgreSQL là 5432
     // database: 'DATN',
-    database: 'DU_AN_TOT_NGHIEP'
+    database: 'DUANTN_2023'
 });
 module.exports = connect;
