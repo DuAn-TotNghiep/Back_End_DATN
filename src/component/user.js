0b17d373dc9afb2b3e1336088cfc76a0711b701e
Duong Van Trung <trungdvph19910@fpt.edu.vn>
2023-09-26 13:39:19 +0700

    Create Api signin

--- src/component/user.js
+++ src/component/user.js
@@ -1,11 +1,11 @@
 const connect = require('../../database');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const { userSchema } = require('../schema/userSchema'); // Đảm bảo bạn đã import schema của người dùng
+const { userSignup, userSignin } = require('../schema/userSchema'); // Đảm bảo bạn đã import schema của người dùng
 
 const Signup = async (req, res) => {
     try {
-        const { error, value } = userSchema.validate(req.body); // Sử dụng schema để kiểm tra dữ liệu
+        const { error, value } = userSignup.validate(req.body); // Sử dụng schema để kiểm tra dữ liệu
 
         if (error) {
             // Nếu dữ liệu không hợp lệ, trả về lỗi cho client
@@ -50,5 +50,56 @@ const Signup = async (req, res) => {
         res.status(500).json({ message: 'Đã xảy ra lỗi' });
     }
 }
+const Signin = async (req, res) => {
+    try {
+        const { user_email, user_password } = req.body;
 
-module.exports = { Signup };
+  
+        const { error } = userSignin.validate(req.body);
+        if (error) {
+            return res.status(400).json({ message: error.details[0].message });
+        }
+
+        // Kiểm tra xem tài khoản tồn tại
+        const checkUserQuery = 'SELECT * FROM users WHERE user_email = $1';
+        const { rows } = await connect.query(checkUserQuery, [user_email]);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ message: 'Tài khoản không tồn tại' });
+        }
+
+        const user = rows[0];
+
+        // Kiểm tra mật khẩu
+        const isPasswordValid = await bcrypt.compare(user_password, user.user_password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Mật khẩu không đúng' });
+        }
+
+        // Tạo AccessToken
+        const accessToken = jwt.sign({ user_id: user.user_id }, 'datn', { expiresIn: '1d' });
+
+        res.status(200).json({
+            message: 'Đăng nhập thành công',
+            user: {
+                id: user.id,
+                user_lastname: user.user_lastname,
+                user_firstname: user.user_firstname,
+                user_province: user.user_province,
+                user_district: user.user_district,
+                user_ward: user.user_ward,
+                user_address: user.user_address,
+                user_image: user.user_image,
+                user_email: user.user_email,
+                user_phone: user.user_phone,
+            },
+            accessToken: accessToken
+        });
+
+    } catch (error) {
+        console.error('Lỗi khi đăng nhập:', error);
+        res.status(500).json({ message: 'Đã xảy ra lỗi' });
+    }
+};
+module.exports = { Signup,Signin };
