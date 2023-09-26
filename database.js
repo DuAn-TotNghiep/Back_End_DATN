ef9888a1d5b0dc53126530b81fc78fa4fcf73177
HoangPham2709 <117618548+HoangPham2709@users.noreply.github.com>
2023-09-26 07:31:51 +0700

    create api removeCategory

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const { Client } = require('pg');
 const connect = new Client({
     user: 'postgres',
-    password: 'admin',
+    password: 'jajaja1234z',
     host: 'localhost',
     port: 5432, // Cổng mặc định của PostgreSQL là 5432
     // port: 3001, // Cổng mặc định của PostgreSQL là 5432
     // database: 'DATN',
-    database: 'DATN_2023_MAIN'
+    database: 'DATN'
 });
 module.exports = connect;
--- src/router/category.js
+++ src/router/category.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { addCategory, getAllCategory } = require("../component/category")
+const { addCategory, getAllCategory, RemoveCategory } = require("../component/category")
 const router = express.Router();
 router.post("/category/add", addCategory);
 router.get('/category', getAllCategory)
+router.delete("/category/:id/remove", RemoveCategory)
 module.exports = router;
