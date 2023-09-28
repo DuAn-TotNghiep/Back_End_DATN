dcbb2da1dd7a42f9c2347194356418b859cf6af2
HoangPham2709 <117618548+HoangPham2709@users.noreply.github.com>
2023-09-29 06:56:59 +0700

    update validate addCategory

--- src/schema/categorySchema.js
+++ src/schema/categorySchema.js
@@ -1,15 +1,24 @@
 const Joi = require('joi');
+
 const categorySchema = Joi.object({
     category_name: Joi.string()
         .trim()
         .min(3)
         .max(30)
         .required()
+        .custom((value, helpers) => {
+            // Kiểm tra xem category_name có chứa toàn số không
+            if (/^\d+$/.test(value)) {
+                return helpers.message({ custom: 'category name không được chứa toàn số' });
+            }
+            return value; // Giá trị hợp lệ
+        })
 }).messages({
     'string.base': 'category name phải là một chuỗi.',
     'string.empty': 'category name không được để trống.',
     'string.min': 'category name phải có ít nhất {#limit} ký tự.',
     'string.max': 'category name không được vượt quá {#limit} ký tự.',
+    'custom': 'category name không được chứa toàn số'
 });
 
 module.exports = { categorySchema };
