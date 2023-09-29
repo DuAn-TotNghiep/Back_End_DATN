9d0a43cf9f7d23ec0b2a49cf33da761172742a01
quan1234 <quanvdph19781@fpt.edu.vn>
2023-09-29 14:42:48 +0700

    Create API GetOneCheckOut

--- src/router/checkout.js
+++ src/router/checkout.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { checkout } = require("../component/checkout");
+const { checkout, getOneheckout } = require("../component/checkout");
 
 const router = express.Router();
 router.post("/checkout/add", checkout);
+router.get("/checkout/:id", getOneheckout);
 
 module.exports = router;
