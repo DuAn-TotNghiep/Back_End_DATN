745bb65269b32a185ad21a816e5d96444e47985e
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-09-30 20:51:17 +0700

    create-api-searchProduct

--- src/router/product.js
+++ src/router/product.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { getAllProducts, AddProduct, RemoveProduct, GetOutstan, GetSale, getNewProduct } = require("../component/product");
+const { getAllProducts, AddProduct, RemoveProduct, GetOutstan, GetSale, getNewProduct, searchProduct } = require("../component/product");
 const router = express.Router();
 router.post("/product/add", AddProduct);
-router.get("/product", getAllProducts)
-router.get("/productnew", getNewProduct)
+router.get("/product", getAllProducts);
+router.get("/productnew", getNewProduct);
+router.get('/product/search', searchProduct)
 router.delete("/product/:id/remove", RemoveProduct);
-router.get("/product/outstan", GetOutstan)
-router.get("/product/sale", GetSale)
+router.get("/product/outstan", GetOutstan);
+router.get("/product/sale", GetSale);
 module.exports = router;
