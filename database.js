601f328a387e33888dcec3de065966e229fd41c2
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-10-03 12:17:12 +0700

    fix-bug-time-order

--- database.js
+++ database.js
@@ -7,10 +7,10 @@ const connect = new Client({
 
     host: 'localhost',
     port: 5432, // Cổng mặc định của PostgreSQL là 5432
-    port: 3001, // Cổng mặc định của PostgreSQL là 5432
-    database: 'DU_AN_TOT_NGHIEP',
+    // port: 3001, // Cổng mặc định của PostgreSQL là 5432
+    // database: 'DATN',
 
-    // database: 'DUANTN'
+    database: 'DATN_2023_MAIN'
 
 
 
