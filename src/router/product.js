763118fc65832887e97872669399d54a7a186e17
quan1234 <quanvdph19781@fpt.edu.vn>
2023-10-03 10:12:50 +0700

    Create API GetOne Product

--- src/router/product.js
+++ src/router/product.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getAllProducts, AddProduct, RemoveProduct, GetOutstan, GetSale, getNewProduct, searchProduct } = require("../component/product");
+const { getAllProducts, AddProduct, RemoveProduct, GetOutstan, GetSale, getNewProduct, searchProduct, GetOneProduct } = require("../component/product");
 const router = express.Router();
 router.post("/product/add", AddProduct);
 router.get("/product", getAllProducts);
@@ -8,4 +8,5 @@ router.get('/product/search', searchProduct)
 router.delete("/product/:id/remove", RemoveProduct);
 router.get("/product/outstan", GetOutstan);
 router.get("/product/sale", GetSale);
+router.get("/product/:id/getone", GetOneProduct);
 module.exports = router;
