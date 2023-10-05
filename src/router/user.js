27d3e4e75a039950248b56fb63f2623fa7d8ecdb
Duong Van Trung <trungdvph19910@fpt.edu.vn>
2023-10-05 15:57:07 +0700

    getone user

--- src/router/user.js
+++ src/router/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { Signup, Signin, TopUser} = require("../component/user");
+const { Signup, Signin, TopUser, GetOneUser } = require("../component/user");
 
 const router = express.Router();
 router.post("/signup", Signup);
 router.post("/signin", Signin);
 router.get("/topuser", TopUser)
+router.get("/user/:id/getone", GetOneUser)
 module.exports = router;
