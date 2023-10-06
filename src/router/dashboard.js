1c484d7b6aaf07b3cf88bbc39a1e7ad9ed11aa28
Duong Van Trung <trungdvph19910@fpt.edu.vn>
2023-10-07 02:29:22 +0700

    create sản phẩm bán chạy trong tháng

--- src/router/dashboard.js
+++ src/router/dashboard.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { getTotalDay, getTotalWeek, TopProductToday, TopProductWeek } = require("../component/dashboard");
+const { getTotalDay, getTotalWeek, TopProductToday, TopProductWeek, TopProductMonth, getTotalMonth } = require("../component/dashboard");
 
 const router = express.Router();
 
 router.get("/totalday", getTotalDay);
 router.get("/totalweek", getTotalWeek);
-router.get("/totalmonth", getTotalWeek);
+router.get("/totalmonth", getTotalMonth);
 router.get("/topproduct", TopProductToday)
 router.get("/topproductweek", TopProductWeek)
+router.get("/topproductmonth", TopProductMonth)
 module.exports = router;
