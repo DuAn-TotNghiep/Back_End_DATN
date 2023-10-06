c575e59b0df070eaa68fb9ac52042c2dc3846a23
vuthinhung1005 <nhungvt19868@fpt.edu.vn>
2023-10-06 13:04:52 +0700

    update usser

--- database.js
+++ database.js
@@ -1,21 +1,18 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
 const connect = new Client({
-    user: 'postgres',
+  user: "postgres",
 
-    // password: 'jajaja1234z',
-    password: 'ADMIN',
+  // password: 'jajaja1234z',
+  password: "admin",
 
-    host: 'localhost',
-    port: 5432, // Cổng mặc định của PostgreSQL là 5432
-    // port: 3001, // Cổng mặc định của PostgreSQL là 5432
-    // database: 'DU_AN_TOT_NGHIEP',
+  host: "localhost",
+  port: 5432, // Cổng mặc định của PostgreSQL là 5432
+  port: 3001, // Cổng mặc định của PostgreSQL là 5432
+  // database: 'DU_AN_TOT_NGHIEP',
 
-    // database: 'DUANTN',
-    // database: 'DATN',
+  // database: 'DUANTN',
+  database: "DATN",
 
-    database: 'DUANTN'
-
-
-
+  // database: "DUANTN",
 });
-module.exports = connect; 
+module.exports = connect;
