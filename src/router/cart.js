8ed450890749ed9b57b35d1f0b150ec34793444f
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-10-18 00:10:33 +0700

    create-api-deleteCart

--- src/router/cart.js
+++ src/router/cart.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const { AddToCart, GetOneCart } = require("../controller/cart");
+const { AddToCart, GetOneCart, deleteCart } = require("../controller/cart");
 const router = express.Router();
 router.post("/cart/addtocart", AddToCart);
 router.get("/cart/:id", GetOneCart);
+router.delete("/cart/:id", deleteCart);
 module.exports = router;
