04ecc8a13ff84aecff76da08e797db72246c7dcb
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-10-21 22:05:10 +0700

    create-api-vnpay-return

--- src/router/vnpay.js
+++ src/router/vnpay.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-const { vnpay } = require("../controller/vnpay");
+const { vnpay, vnpay_return } = require("../controller/vnpay");
 router.post('/vnpay', vnpay);
+router.get('/vnpay_return', vnpay_return)
 
 module.exports = router;
