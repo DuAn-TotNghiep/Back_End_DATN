265d2cf9264b0c1238d966caa5d4df5019b32900
quan1234 <quanvdph19781@fpt.edu.vn>
2023-10-21 19:49:35 +0700

    update add bill

--- src/controller/bill.js
+++ src/controller/bill.js
@@ -22,7 +22,7 @@ const getBill = async (req, res) => {
 const getOneBill = async (req, res) => {
     try {
         const { id } = req.params
-        const sql = `SELECT * FROM bill WHERE bill_id = ${id}`
+        const sql = `SELECT * FROM bill WHERE checkout_id = ${id} RETURNING*`
         connect.query(sql, (err, results) => {
             if (err) {
                 return res.status(500).json({ message: 'Lấy 1 id thất bại', err })
@@ -52,34 +52,43 @@ const getOneBillInCheckOut = async (req, res) => {
 const bill = async (req, res) => {
     try {
         const { user_id, order_id } = req.body;
+
         const bill_date = DateTime.local().setZone('Asia/Ho_Chi_Minh');
-        const sql = {
-            text: 'INSERT INTO bill (user_id, order_id, bill_date) VALUES ($1, $2, $3) RETURNING *',
-            values: [user_id, order_id, bill_date],
-        };
-        const result = await connect.query(sql);
+        const sql =
+            `INSERT INTO bill (user_id, order_id, bill_date) VALUES (${user_id}, ${order_id}, '${bill_date}') RETURNING *`
 
-        if (result.rowCount > 0) {
-            // Lấy thông tin từ bảng "orders"
-            const getOrdersSql = {
-                text: 'SELECT payment_status, order_total FROM orders WHERE order_id = $1',
-                values: [order_id],
-            };
-            const ordersResult = await connect.query(getOrdersSql);
-
-            if (ordersResult.rowCount > 0) {
-                const ordersData = ordersResult.rows[0];
-                const data = result.rows[0];
-                // Thêm thông tin từ bảng "orders" vào đối tượng JSON
-                data.payment_status = ordersData.payment_status;
-                data.order_total = ordersData.order_total;
-                return res.status(200).json({ message: 'Tạo  bill thành công', data });
-            } else {
-                return res.status(500).json({ message: 'Không thể lấy thông tin từ bảng orders' });
+        connect.query(sql, (err, result) => {
+            if(err){
+                return res.status(500).json({message: "loi them bill",err})
             }
-        } else {
-            return res.status(500).json({ message: 'Tạo bill thất bại' });
-        }
+            const data = result.rows
+            console.log(data);
+            return res.status(200).json({ message: 'Tạo  bill thành công', data });
+        })
+
+        
+       
+        // if (result.rowCount > 0) {
+        //     // Lấy thông tin từ bảng "orders"
+        //     const getOrdersSql = {
+        //         text: 'SELECT payment_status, order_total FROM orders WHERE order_id = $1',
+        //         values: [order_id],
+        //     };
+        //     const ordersResult = await connect.query(getOrdersSql);
+
+        //     if (ordersResult.rowCount > 0) {
+        //         const ordersData = ordersResult.rows[0];
+        //         const data = result.rows[0];
+        //         // Thêm thông tin từ bảng "orders" vào đối tượng JSON
+        //         data.payment_status = ordersData.payment_status;
+        //         data.order_total = ordersData.order_total;
+        //         return res.status(200).json({ message: 'Tạo  bill thành công', data });
+        //     } else {
+        //         return res.status(500).json({ message: 'Không thể lấy thông tin từ bảng orders' });
+        //     }
+        // } else {
+        //     return res.status(500).json({ message: 'Tạo bill thất bại' });
+        // }
     } catch (error) {
         return res.status(500).json({ message: 'Lỗi API' });
     }
