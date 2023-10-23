baf03a2becdc4f783f1c01d1d28fc4761c5e1452
quan1234 <quanvdph19781@fpt.edu.vn>
2023-10-24 03:02:14 +0700

    update saleproduct

--- src/router/sale.js
+++ src/router/sale.js
@@ -2,8 +2,9 @@
 
 
 const express = require("express");
-const { getAllSale } = require("../controller/sale");
+const { getAllSale, updateSaleProduct } = require("../controller/sale");
 const router = express.Router();
 
 router.get("/sale", getAllSale);
+router.patch("/sale/:id/updatesaleproduct", updateSaleProduct);
 module.exports = router;
