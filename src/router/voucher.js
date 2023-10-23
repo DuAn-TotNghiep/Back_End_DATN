07560acb892014267150a4f0573fedc4c6111fa6
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-10-24 04:46:53 +0700

    create-api-crud-voucher

--- src/router/voucher.js
+++ src/router/voucher.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { voucher, AddVoucher } = require("../controller/voucher");
+const { voucher, AddVoucher, DeleteVoucher, UpdateVoucher } = require("../controller/voucher");
 
 const router = express.Router();
 router.post('/voucher', voucher)
 router.post('/voucher/add', AddVoucher)
+router.delete('/voucher/:id', DeleteVoucher)
+router.patch('/voucher/:id/update', UpdateVoucher)
 
 module.exports = router;
