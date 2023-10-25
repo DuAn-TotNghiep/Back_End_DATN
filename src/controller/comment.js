064350cffa38f47899d7101b1beb0601c13ab026
quan1234 <quanvdph19781@fpt.edu.vn>
2023-10-25 07:29:14 +0700

    updaste cmt

--- src/controller/comment.js
+++ src/controller/comment.js
@@ -3,20 +3,32 @@ const { DateTime } = require('luxon');
 const AddComment = (req, res) => {
     try {
         const { content, user_id, product_id } = req.body;
-        const comment_date = DateTime.local().setZone('Asia/Ho_Chi_Minh').toISO();
-        const sql = `INSERT INTO comment(product_id, user_id, comment_text, comment_date) VALUES (${product_id}, ${user_id}, '${content}', '${comment_date}') RETURNING *`;
-
-        connect.query(sql, (err, result) => {
-            if (err) {
-                return res.status(500).json({ message: "Khong them duoc cmt", err })
+        // Thực hiện kiểm tra xem người dùng đã có nhận xét cho sản phẩm này chưa
+        const checkSql = `SELECT * FROM comment WHERE product_id = ${product_id} AND user_id = ${user_id}`;
+        connect.query(checkSql, (checkErr, checkResult) => {
+            if (checkErr) {
+                return res.status(500).json({ message: "Lỗi khi kiểm tra nhận xét", err: checkErr });
             }
-            const data = result.rows[0]
-            return res.status(200).json({ message: "Them Comment thanh cong", data })
-        })
+            if (checkResult.rows.length > 0) {
+                return res.status(400).json({ message: "Bạn đã bình luận cho sản phẩm này" });
+            }
+
+            // Nếu người dùng chưa có nhận xét, thực hiện thêm nhận xét mới vào cơ sở dữ liệu
+            const comment_date = DateTime.local().setZone('Asia/Ho_Chi_Minh').toISO();
+            const sql = `INSERT INTO comment(product_id, user_id, comment_text, comment_date) VALUES (${product_id}, ${user_id}, '${content}', '${comment_date}') RETURNING *`;
+            connect.query(sql, (err, result) => {
+                if (err) {
+                    return res.status(500).json({ message: "Không thêm được nhận xét", err });
+                }
+                const data = result.rows[0];
+                return res.status(200).json({ message: "Thêm Comment thành công", data });
+            });
+        });
     } catch (err) {
-        return res.status(404).json({ message: 'Loi api' });
+        return res.status(404).json({ message: 'Lỗi API' });
     }
 }
+
 const GetAllCommentProduct = (req, res) => {
     try {
         const { id } = req.params
