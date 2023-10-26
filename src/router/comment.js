7a260975f3d03c259c25d4fb55048e60c00b8b72
Đào Xuân Mai - FE <118628374+mailuoicode@users.noreply.github.com>
2023-10-26 23:19:17 +0700

    create-api-deleteComment

--- src/router/comment.js
+++ src/router/comment.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { AddComment, GetAllCommentProduct, getAllComment } = require("../controller/comment");
+const { AddComment, GetAllCommentProduct, getAllComment, deleteComment } = require("../controller/comment");
 const router = express.Router();
 router.post("/comment/add", AddComment);
 router.get("/comment/:id/product", GetAllCommentProduct);
 router.get("/comment", getAllComment);
+router.delete("/comment/:id", deleteComment);
 module.exports = router;
