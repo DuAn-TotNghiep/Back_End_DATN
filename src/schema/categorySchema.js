df8194532d56d5d26097d80286823e30e7754a73
Duong Van Trung <trungdvph19910@fpt.edu.vn>
2023-11-06 16:57:34 +0700

    thêm image category

--- src/schema/categorySchema.js
+++ src/schema/categorySchema.js
@@ -12,13 +12,20 @@ const categorySchema = Joi.object({
                 return helpers.message({ custom: 'category name không được chứa toàn số' });
             }
             return value; // Giá trị hợp lệ
+        }),
+        image: Joi.required().messages({
+            "string.empty": "Không được để trống image",
+            "any.required": "Trường image phải là bắt buộc",
+          
         })
+        
 }).messages({
-    'string.base': 'category name phải là một chuỗi.',
+    // 'string.base': 'category name phải là một chuỗi.',
     'string.empty': 'category name không được để trống.',
     'string.min': 'category name phải có ít nhất {#limit} ký tự.',
     'string.max': 'category name không được vượt quá {#limit} ký tự.',
-    'custom': 'category name không được chứa toàn số'
+    'custom': 'category name không được chứa toàn số',
+
 });
 
 module.exports = { categorySchema };
