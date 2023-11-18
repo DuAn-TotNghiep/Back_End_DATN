b8a81bfaf77015fcb6645dee7d2ed7944fc97e87
trungdvph19910 <trungdvph19910@fpt.edu.vn>
2023-11-18 13:35:31 +0700

    getone sale

--- src/controller/sale.js
+++ src/controller/sale.js
@@ -18,6 +18,22 @@ const getAllSale = async (req, res) => {
         return res.status(500).json({ message: 'Lỗi API' });
     }
 }
+const getOneSale = async (req, res) => {
+    try {
+        const { id } = req.params
+        const sql = `SELECT * FROM sale WHERE sale_id = ${id}`
+        connect.query(sql, (err, results) => {
+            if (err) {
+                return res.status(500).json({ message: 'Lay one that bai', err })
+            }
+            const data = results.rows
+            return res.status(200).json({ message: 'Lay one thanh cong', data })
+        })
+    } catch (err) {
+        return res.status(500).json({ message: 'Loi api', err })
+    }
+}
+
 const updateSaleProduct = async (req, res) => {
     try {
         const { sale_id, id } = req.body;
@@ -132,4 +148,4 @@ const RemoveSale = async (req, res) => {
     }
 };
 
-module.exports = { updateSale,getAllSale, updateSaleProduct ,addSale, RemoveSale};
+module.exports = {getOneSale, updateSale,getAllSale, updateSaleProduct ,addSale, RemoveSale};
--- src/router/sale.js
+++ src/router/sale.js
@@ -2,10 +2,11 @@
 
 
 const express = require("express");
-const { getAllSale, updateSaleProduct ,addSale, updateSale, RemoveSale} = require("../controller/sale");
+const { getAllSale, updateSaleProduct ,addSale, updateSale, RemoveSale, getOneSale} = require("../controller/sale");
 const router = express.Router();
 
 router.get("/sale", getAllSale);
+router.get("/sale/:id", getOneSale);
 router.patch("/sale/updatesaleproduct", updateSaleProduct);
 router.patch("/sale/:id/update", updateSale);
 router.delete("/sale/:id/remove", RemoveSale)
