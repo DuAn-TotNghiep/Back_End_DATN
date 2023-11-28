cb599bbda0be0f50346d5ad476c5c01b18518d41
quan1234 <quanvdph19781@fpt.edu.vn>
2023-11-28 12:07:28 +0700

    fix update 999

--- database.js
+++ database.js
@@ -15,7 +15,7 @@ const connect = new Client({
 
   host: "localhost",
   port: 5432, // Cổng mặc định của PostgreSQL là 5432
-  // port: 3001, // Cổng mặc định của PostgreSQL là 5432
+  port: 3001, // Cổng mặc định của PostgreSQL là 5432
 
   // port: 3001, // Cổng mặc định của PostgreSQL là 5432
   // database: 'DU_AN_TOT_NGHIEP',
@@ -23,10 +23,10 @@ const connect = new Client({
   // database: "DATN_MAIN",
 
   // database: "DATN1",
-  // database: "DATN2",
+  database: "DATN",
 
   // database: "DATN_2023_MAIN",
-  database: "DUAN",
+  // database: "DUAN",
 
   // database: "DATN_2023_MAIN"
 });
