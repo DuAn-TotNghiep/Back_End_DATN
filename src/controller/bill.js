44bc3a3c6498a51b6f8e116f56883cb967b6d7b5
quan1234 <quanvdph19781@fpt.edu.vn>
2023-12-05 21:09:38 +0700

    update bill

--- src/controller/bill.js
+++ src/controller/bill.js
@@ -1,6 +1,7 @@
 const { DateTime } = require("luxon");
 const connect = require("../../database");
 const { BillSchema } = require("../schema/BillSchema");
+const { getAllSale } = require("./sale");
 
 const getBill = async (req, res) => {
     try {
@@ -132,44 +133,101 @@ const getOneBillInCheckOut = async (req, res) => {
 const bill = async (req, res) => {
     try {
         const { user_id, order_id } = req.body;
-
         const bill_date = DateTime.local().setZone("Asia/Ho_Chi_Minh");
-        const sql = `INSERT INTO bill (user_id, order_id, bill_date) VALUES (${user_id}, ${order_id}, '${bill_date}') RETURNING *`;
 
-        connect.query(sql, (err, result) => {
+        const userQuery = `SELECT * FROM users WHERE id = ${user_id}`;
+        const userData = await connect.query(userQuery);
+        const user = userData.rows[0];
+        const lastName = user?.user_lastname;
+        const firstName = user?.user_firstname;
+        const phone = user?.user_phone;
+
+        const orderQuery = `SELECT * FROM orders WHERE order_id = ${order_id}`;
+        const orderData = await connect.query(orderQuery);
+        const order = orderData.rows[0];
+        const totalSum = order?.order_total;
+
+        const checkoutQuery = `SELECT * FROM checkout WHERE id = ${order?.checkout_id}`;
+        const checkoutData = await connect.query(checkoutQuery);
+        const checkout = checkoutData.rows[0];
+        const address = checkout?.address;
+        const province = checkout?.province;
+        const district = checkout?.district;
+        const ward = checkout?.ward;
+
+        const productQueryPromises = checkout?.product?.map(async (data) => {
+            const productQuery = `SELECT * FROM product WHERE product_id = ${data?.product_id}`;
+
+            const productResult = await connect.query(productQuery);
+            const productData = productResult.rows[0];
+            let sql = `SELECT * FROM sale`;
+            const sale = await connect.query(sql);
+            const salediscount = sale?.rows?.find((data) => data?.sale_id == productData?.sale_id)?.sale_distcount
+            const price = productData?.product_price * (salediscount/100)
+            if (productData?.sale_id !=null) {
+                return {
+                    size: data?.size,
+                    color: data?.color,
+                    quantity: data?.quantity,
+                    name: productData?.product_name,
+                    price: productData?.product_price - price
+                };
+            } else {
+                return {
+                    size: data?.size,
+                    color: data?.color,
+                    quantity: data?.quantity,
+                    name: productData?.product_name,
+                    price: productData?.product_price
+                };
+            }
+        });
+
+        const productData = await Promise.all(productQueryPromises);
+        const productsJson = JSON.stringify(productData);
+        const sql1 = `
+    INSERT INTO bill (
+        customer_name, 
+        customer_last_name, 
+        customer_address,
+        customer_province, 
+        customer_district, 
+        customer_ward, 
+        customer_phone, 
+        total_amount,
+        creation_time, 
+        products,
+        order_id
+    ) 
+    VALUES (
+        '${firstName}', 
+        '${lastName}', 
+        '${address}', 
+        '${province}', 
+        '${district}', 
+        '${ward}', 
+        '${phone}', 
+        '${totalSum}', 
+        '${bill_date}', 
+        '${productsJson}',
+        ${order_id}
+    ) 
+    RETURNING *
+`;
+
+        connect.query(sql1, (err, result) => {
             if (err) {
                 return res.status(500).json({ message: "loi them bill", err });
             }
             const data = result.rows;
-            console.log(data);
-            return res.status(200).json({ message: "Tạo  bill thành công", data });
+            return res.status(200).json({ message: "Tạo bill thành công", data });
         });
-
-        // if (result.rowCount > 0) {
-        //     // Lấy thông tin từ bảng "orders"
-        //     const getOrdersSql = {
-        //         text: 'SELECT payment_status, order_total FROM orders WHERE order_id = $1',
-        //         values: [order_id],
-        //     };
-        //     const ordersResult = await connect.query(getOrdersSql);
-
-        //     if (ordersResult.rowCount > 0) {
-        //         const ordersData = ordersResult.rows[0];
-        //         const data = result.rows[0];
-        //         // Thêm thông tin từ bảng "orders" vào đối tượng JSON
-        //         data.payment_status = ordersData.payment_status;
-        //         data.order_total = ordersData.order_total;
-        //         return res.status(200).json({ message: 'Tạo  bill thành công', data });
-        //     } else {
-        //         return res.status(500).json({ message: 'Không thể lấy thông tin từ bảng orders' });
-        //     }
-        // } else {
-        //     return res.status(500).json({ message: 'Tạo bill thất bại' });
-        // }
     } catch (error) {
+        console.error(error);
         return res.status(500).json({ message: "Lỗi API" });
     }
 };
+
 const getOneBill2 = async (req, res) => {
     try {
         const { id } = req.params;
